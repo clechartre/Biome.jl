fe36ac139677f4723324c9c0354c9506c0ee8357
Capucine Lechartre <capucine.lechartre@gmail.com>
2025-07-16 17:13:15 +0200

    extra PFTs to compose with

--- src/models/MechanisticModel/pfts.jl
+++ src/models/MechanisticModel/pfts.jl
@@ -5,18 +5,20 @@ abstract type AbstractTropicalPFT <: AbstractPFT end
 abstract type AbstractTemperatePFT <: AbstractPFT end
 abstract type AbstractBorealPFT <: AbstractPFT end
 abstract type AbstractGrassPFT <: AbstractPFT end
+abstract type AbstractDeciduousPFT <: AbstractPFT end
+abstract type AbstractEvergreenPFT <: AbstractPFT end
 
 @kwdef mutable struct PFTCharacteristics{T<:Real,U<:Int} <: AbstractPFTCharacteristics
     name::String = "Default"
     phenological_type::U = U(1)
     max_min_canopy_conductance::T = T(0.0)
-    Emax::T = T(0.0)
-    sw_drop::T = T(0.0)
-    sw_appear::T = T(0.0)
+    Emax::T = T(10.0)
+    sw_drop::T = T(-99.9)
+    sw_appear::T = T(-99.9)
     root_fraction_top_soil::T = T(0.0)
     leaf_longevity::T = T(0.0)
-    GDD5_full_leaf_out::T = T(1.0)
-    GDD0_full_leaf_out::T = T(1.0)
+    GDD5_full_leaf_out::T = T(-99.9)
+    GDD0_full_leaf_out::T = T(-99.9)
     sapwood_respiration::U = U(1)
     optratioa::T = T(1.0)
     kk::T = T(1.0)
@@ -72,26 +74,18 @@ PFTCharacteristics() = PFTCharacteristics{Float64,Int}()
     lai::T = zero(T)
 end
 
-
-# Base constructors ----------------------------------------------------
+# Base constructors with minimal required plus constraints ----------------
 function base_tropical_pft(::Type{T}, ::Type{U}; kwargs...) where {T<:Real,U<:Int}
     defaults = (
         max_min_canopy_conductance = T(0.5),
         Emax = T(10.0),
-        sw_drop = T(-99.9),
-        sw_appear = T(-99.9),
         root_fraction_top_soil = T(0.69),
-        GDD5_full_leaf_out = T(-99.9),
-        GDD0_full_leaf_out = T(-99.9),
-        sapwood_respiration = U(1),
         optratioa = T(0.95),
         kk = T(0.7),
-        c4 = false,
         t0 = T(10.0),
         tcurve = T(1.0),
         respfact = T(0.8),
         allocfact = T(1.0),
-        grass = false,
         constraints = (
             tcm = [-Inf, +Inf],
             min = [T(0.0), +Inf],
@@ -106,41 +100,33 @@ function base_tropical_pft(::Type{T}, ::Type{U}; kwargs...) where {T<:Real,U<:In
     return PFTCharacteristics{T,U}(; name = "TropicalBase", merged...)
 end
 
-# Concrete PFT types with characteristics
 struct TropicalBase{T<:Real,U<:Int} <: AbstractTropicalPFT
     characteristics::PFTCharacteristics{T,U}
     state::PFTState{T,U}
 end
 
-# Constructors
 TropicalBase(::Type{T}=Float64, ::Type{U}=Int; kwargs...) where {T<:Real,U<:Int} =
     TropicalBase(base_tropical_pft(T, U; kwargs...), PFTState{T,U}())
 
-
 function base_temperate_pft(::Type{T}, ::Type{U}; kwargs...) where {T<:Real,U<:Int}
     defaults = (
-        Emax = T(5),
-        sw_drop = T(-99.9),
-        sw_appear = T(-99.9),
+        Emax = T(5.0),
         root_fraction_top_soil = T(0.67),
-        GDD5_full_leaf_out = T(-99.9),
-        GDD0_full_leaf_out = T(-99.9),
-        sapwood_respiration = U(1),
-        optratioa = T(0.8),
-        kk = T(0.6),
-        c4 = false,
+        GDD5_full_leaf_out = T(200.0),
+        optratioa = T(0.80),
+        kk = T(0.65),
+        t0 = T(5.0),
         tcurve = T(1.0),
         respfact = T(1.45),
         allocfact = T(1.2),
-        grass = false,
         constraints = (
-            tcm = [T(-15.0), +Inf],
-            min = [-Inf, T(5.0)],
-            gdd = [T(900), +Inf],
-            gdd0 = [-Inf, +Inf],
-            twm = [T(10), +Inf],
-            snow = [-Inf, +Inf],
-            swb = [T(300), +Inf]
+            tcm   = [T(-15.0), +Inf],
+            min   = [T(-Inf), T(5.0)],
+            gdd   = [T(900.0), +Inf],
+            gdd0  = [T(-Inf), +Inf],
+            twm   = [T(-Inf), +Inf],
+            snow  = [T(-Inf), +Inf],
+            swb   = [T(300.0), +Inf]
         )
     )
     merged = merge(defaults, kwargs)
@@ -155,34 +141,26 @@ end
 TemperateBase(::Type{T}=Float64, ::Type{U}=Int; kwargs...) where {T<:Real,U<:Int} =
     TemperateBase(base_temperate_pft(T, U; kwargs...), PFTState{T,U}())
 
-
 function base_boreal_pft(::Type{T}, ::Type{U}; kwargs...) where {T<:Real,U<:Int}
     defaults = (
-        Emax = T(8),
-        sw_drop = T(-99.9),
-        sw_appear = T(-99.9),
+        Emax = T(4.5),
         root_fraction_top_soil = T(0.83),
         leaf_longevity = T(24.0),
-        GDD5_full_leaf_out = T(-99.9),
-        GDD0_full_leaf_out = T(-99.9),
-        sapwood_respiration = U(1),
-        optratioa = T(0.85),
-        kk = T(0.45),
-        c4 = false,
+        optratioa = T(0.90),
+        kk = T(0.40),
         threshold = T(0.33),
         t0 = T(0.0),
         tcurve = T(0.8),
         respfact = T(4.0),
         allocfact = T(1.2),
-        grass = false,
         constraints = (
-            tcm = [-Inf, T(5.0)],
-            min = [-Inf, +Inf],
-            gdd = [-Inf, +Inf],
-            gdd0 = [-Inf, +Inf],
-            twm = [-Inf, T(21.0)],
-            snow = [-Inf, +Inf],
-            swb = [T(300), +Inf]
+            tcm   = [T(-Inf), T(5.0)],
+            min   = [T(-Inf), +Inf],
+            gdd   = [T(-Inf), +Inf],
+            gdd0  = [T(-Inf), +Inf],
+            twm   = [T(-Inf), T(21.0)],
+            snow  = [T(-Inf), +Inf],
+            swb   = [T(300.0), +Inf]
         )
     )
     merged = merge(defaults, kwargs)
@@ -201,11 +179,10 @@ function base_grass_pft(::Type{T}, ::Type{U}; kwargs...) where {T<:Real,U<:Int}
     defaults = (
         phenological_type = U(3),
         max_min_canopy_conductance = T(0.8),
-        Emax = T(7.25),
-        sw_drop = T(0.2),
-        sw_appear = T(0.3),
-        GDD5_full_leaf_out = T(-99.9),
-        GDD0_full_leaf_out = T(-99.9),
+        Emax = T(6.5),
+        sw_drop = T(0.20),
+        sw_appear = T(0.30),
+        leaf_longevity = T(8.0),
         sapwood_respiration = U(2),
         optratioa = T(0.65),
         kk = T(0.4),
@@ -214,14 +191,14 @@ function base_grass_pft(::Type{T}, ::Type{U}; kwargs...) where {T<:Real,U<:Int}
         allocfact = T(1.0),
         grass = true,
         constraints = (
-            tcm = [-Inf, +Inf],
-            min = [-Inf, +Inf],
-            gdd = [-Inf, +Inf],
-            gdd0 = [-Inf, +Inf],
-            twm = [-Inf, +Inf],
-            snow = [-Inf, +Inf],
-            swb = [T(100), +Inf]
-        ),
+            tcm   = [T(-Inf), +Inf],
+            min   = [T(-Inf), +Inf],
+            gdd   = [T(-Inf), +Inf],
+            gdd0  = [T(-Inf), +Inf],
+            twm   = [T(-Inf), +Inf],
+            snow  = [T(-Inf), +Inf],
+            swb   = [T(100.0), +Inf]
+        )
     )
     merged = merge(defaults, kwargs)
     return PFTCharacteristics{T,U}(; name = "GrassBase", merged...)
@@ -235,11 +212,55 @@ end
 GrassBase(::Type{T}=Float64, ::Type{U}=Int; kwargs...) where {T<:Real,U<:Int} =
     GrassBase(base_grass_pft(T, U; kwargs...), PFTState{T,U}())
 
+function base_deciduous_pft(::Type{T}, ::Type{U}; kwargs...) where {T<:Real,U<:Int}
+    defaults = (
+        phenological_type = U(2),
+        leaf_longevity = T(7.0),
+        GDD5_full_leaf_out = T(200.0),
+        kk = T(0.65)
+    )
+    merged = merge(defaults, kwargs)
+    return PFTCharacteristics{T,U}(; name = "DeciduousBase", merged...)
+end
+
+struct DeciduousBase{T<:Real,U<:Int} <: AbstractDeciduousPFT
+    characteristics::PFTCharacteristics{T,U}
+    state::PFTState{T,U}
+end
+
+DeciduousBase(::Type{T}=Float64, ::Type{U}=Int; kwargs...) where {T<:Real,U<:Int} =
+    DeciduousBase(base_deciduous_pft(T, U; kwargs...), PFTState{T,U}())
+
+function base_evergreen_pft(::Type{T}, ::Type{U}; kwargs...) where {T<:Real,U<:Int}
+    defaults = (
+        phenological_type = U(1),
+        leaf_longevity = T(18.0),
+        kk = T(0.67)
+    )
+    merged = merge(defaults, kwargs)
+    return PFTCharacteristics{T,U}(; name = "EvergreenBase", merged...)
+end
+
+struct EvergreenBase{T<:Real,U<:Int} <: AbstractEvergreenPFT
+    characteristics::PFTCharacteristics{T,U}
+    state::PFTState{T,U}
+end
+
+EvergreenBase(::Type{T}=Float64, ::Type{U}=Int; kwargs...) where {T<:Real,U<:Int} =
+    EvergreenBase(base_evergreen_pft(T, U; kwargs...), PFTState{T,U}())
+
+# Utility and classification ----------------------------------------------
+
+"""
+    get_characteristic(pft, prop)
+
+Fetch a named characteristic field from a PFT’s characteristics.
+"""
 function get_characteristic(pft::AbstractPFT, prop::Symbol)
     if hasproperty(pft.characteristics, prop)
         return getproperty(pft.characteristics, prop)
     else
-        throw(ArgumentError("`$(prop)` is not a field of PFTCharacteristics"))
+        throw(ArgumentError("`$(prop)` is not a PFTCharacteristics field"))
     end
 end
 
@@ -255,6 +276,11 @@ end
 None{T,U}() where {T<:Real,U<:Int} = None{T,U}(PFTCharacteristics{T,U}())
 None() = None{Float64,Int}()
 
+"""
+    PFTClassification()
+
+Return a list of all base PFT instances.
+"""
 struct PFTClassification{T<:Real,U<:Int} <: AbstractPFTList
     pft_list::Vector{AbstractPFT}
 end
@@ -265,6 +291,8 @@ function PFTClassification{T,U}() where {T<:Real,U<:Int}
         TemperateBase{T,U}(),
         BorealBase{T,U}(),
         GrassBase{T,U}(),
+        DeciduousBase{T,U}(),
+        EvergreenBase{T,U}(),
     ])
 end
 PFTClassification() = PFTClassification{Float64,Int}()
@@ -272,15 +300,11 @@ PFTClassification() = PFTClassification{Float64,Int}()
 """
     dominance_environment(pft, variable, clt)
 
-Calculate the normalized environmental dominance value for a given climate trait.
-
-Returns a value in [0,1], where 1 is at the species' optimal mean and values fall off according to its tolerance.
+Calculate environmental dominance (0–1) given climate variable.
 """
-function dominance_environment(pft::AbstractPFT, variable::Symbol, clt)
-    mean_val = get_characteristic(pft, :mean_val)[variable]
-    std_val  = get_characteristic(pft, :sd_val)[variable]
-    dist     = Normal(mean_val, std_val)
-    pdf_clt  = pdf(dist, clt)
-    pdf_mean = pdf(dist, mean_val)
-    return pdf_clt / pdf_mean
+function dominance_environment(pft::AbstractPFT, variable::Symbol, clt::Real)
+    mv = get_characteristic(pft, :mean_val)[variable]
+    sd = get_characteristic(pft, :sd_val)[variable]
+    dist = Normal(mv, sd)
+    return pdf(dist, clt) / pdf(dist, mv)
 end
